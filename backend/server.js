51e9418258b420f129d79326b72964fef2a826e5
Shivendra Techster <shivendratechster@gmail.com>
2021-06-12 12:03:11 +0530

    error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectToDb from "./config/db.js";
 import productRoutes from './routes/productRoutes.js'
 
@@ -10,5 +11,9 @@ connectToDb()
 const app = express()
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.listen(process.env.PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`))
